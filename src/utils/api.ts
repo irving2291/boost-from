3ae0c5e516b563ed8ba8257d99f8cacd73bffb46
irving Jacome <irving2291@gmail.com>
2025-08-27 12:25:03 -0500

    update route

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,5 +1,5 @@
 // API Configuration
-export const API_BASE_URL = ((import.meta as any)?.env?.VITE_API_BASE_URL as string) || 'http://localhost:8080'
+export const API_BASE_URL = ((import.meta as any)?.env?.VITE_API_BASE_URL as string) || 'https://api.boost.pitahayasoft.com/v1'
 
 // API Endpoints
 export const API_ENDPOINTS = {
